850d8367aab34363d31e490cb01e4a0eb3503e2a
18483629341 <714402934@qq.com>
2019-04-28 21:01:08 +0800

    公共样式文件配置成功

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  public a = ' a ';
+  public list = [
+    {
+      ' RL_TYPE ': 'HL',
+      ' opi' : [
+        {
+          ' W01019': 1.7,
+          'WATER_TYPE': '1',
+          'WATER_TYPE_JB': 'I',
+          'W01009': 8,
+          'W21003': 0.13,
+          'WQ_AM_PI_NAME': '天津三岔口',
+          'W21011': null,
+          'W01001': 7.3,
+          'W21001': null
+        }
+      ],
+      'RLNAME': '海河',
+      'RL_WATER_TYPE': '1',
+      'RL_WATER_TYPE_JB': 'I',
+      'MTIME': '2018/5/30 21:00'
+    },
+    {
+      'RL_TYPE': 'HL',
+      'opi': [
+        {
+          'W01019': 1.9,
+          'WATER_TYPE': '2',
+          'WATER_TYPE_JB': 'II',
+          'W01009': 6.9,
+          'W21003': 0.26,
+          'WQ_AM_PI_NAME': '天津果河桥',
+          'W21011': 0.05,
+          'W01001': 8.5,
+          'W21001': 2.82
+        }
+      ],
+      'RLNAME': '黎河',
+      'RL_WATER_TYPE': '2',
+      'RL_WATER_TYPE_JB': 'II',
+      'MTIME': '2018/5/30 21:00'
+    }
+  ]
+  constructor() {
+
+  }
+
+  // this.getSectionList();
+
+  getSectionList() {
+    // this.homeService.getSectionList({} , true , (res) => {
+    // console.log(res);
+    // });
+  }
+}
